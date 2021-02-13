2b3eeaa7852d1d3a56778dfcf96cebe11a90c10f
Alex Quinn <alex@wundercapital.com>
2021-02-13 13:31:30 -0700

    Add TMUX

--- setup.sh
+++ setup.sh
@@ -2,19 +2,28 @@
 
 set -euo pipefail
 
+dotfiles_dir=$(cd "$(dirname "$0")"; pwd)
+
 #### Vim ####
 
 rm -rf "${HOME}/.vim"
 rm -f "${HOME}/.vimrc"
 
-vim_dotfiles_dir=$(cd "$(dirname "$0")"; pwd)
 mkdir "${HOME}/.vim"
-ln -s "${vim_dotfiles_dir}/vim/vimrc" "${HOME}/.vimrc"
-ln -s "${vim_dotfiles_dir}/vim/vimrc.bundles" "${HOME}/.vim/vimrc.bundles"
-ln -s "${vim_dotfiles_dir}/vim/ruby_mappings.vim" "${HOME}/.vim/ruby_mappings.vim"
-ln -s "${vim_dotfiles_dir}/vim/colors/" "${HOME}/.vim/colors"
+ln -s "${dotfiles_dir}/vim/vimrc" "${HOME}/.vimrc"
+ln -s "${dotfiles_dir}/vim/vimrc.bundles" "${HOME}/.vim/vimrc.bundles"
+ln -s "${dotfiles_dir}/vim/ruby_mappings.vim" "${HOME}/.vim/ruby_mappings.vim"
+ln -s "${dotfiles_dir}/vim/colors/" "${HOME}/.vim/colors"
 
 # Install vim-plug
 curl -sfLo "${HOME}/.vim/autoload/plug.vim" --create-dirs "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
 
 vim +PlugInstall +PlugClean! +qall
+
+#### TMUX ####
+
+rm -f "${HOME}/.tmux.conf"
+rm -f "${HOME}/.tmate.conf"
+
+ln -s "${dotfiles_dir}/tmux/tmux.conf" "${HOME}/.tmux.conf"
+ln -s "${dotfiles_dir}/tmux/tmate.conf" "${HOME}/.tmate.conf"
